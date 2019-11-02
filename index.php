48accfbcabb8da7eebe2141d407eae1cca93e46c
UnderdogDE <31598023+UnderdogDE@users.noreply.github.com>
2019-11-02 19:05:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,55 @@
 <head>
 <title>owo</title>
 <meta charset='utf-8'>
-<meta name='viewport' content='width=device-width, initial-scale=1'>
 <style>
-body{background-image: url('bg.jpg');background-repeat: repeat-y-x;}
-img{border-radius:15px;height:200px;padding:5px;}
-.center{ width:90%;margin-left:auto;margin-right:auto;}
+body{background-image: url('bg.jpg');
+background-repeat: repeat-y-x;
+}
+
+img{border-radius:15px;height:150px;
+padding:5px;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto auto ;
+  padding: 0px;
+  grid-gap: 10px;
+  width: auto;
+  height: auto;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.grid-item {
+  background-color: #b6b6b64f;
+  border: 1px solid #5a5a5acc;
+  border-radius: 20px;
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+  height: auto;
+  width: auto;
+}
+
+a.date {
+  background-color: #222222;
+  color: #ffffff;
+  padding: 5px;
+  border-radius: 20px;
+}
 </style>
 </head>
+
 <body>
 <div class='center'>
+<div class='grid-container'>
 <?php $files = scandir('img/');
     foreach($files as $file) {
         if($file !== "." && $file !== "..") {
-echo "<a href='img/$file'target='_blank'><img src='img/$file'/></a>";}}?>
+echo "<div id='$file' class='grid-item'><a href='img/$file'target='_blank'><img src='img/$file'/></a></div>";}}?>
 </div>
+<br>
+<center><a class='date'> &copy; 2018-<?php echo date("Y");?></a></center>
 </body>
-</html>
+</html>>
